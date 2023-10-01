d4d9b0ebaf317bbbd0dcada197f0e809f8d121ac
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-09-30 20:23:54 -0500

    added the list view with the images(loading) and free

--- Exercise5_waobikeze_Brian/ContentView.swift
+++ Exercise5_waobikeze_Brian/ContentView.swift
@@ -7,20 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct response: Codable{
+    var results: [restrunts]
+}
+struct ContentView: View {
+    @State private var Restrunts = [restrunts]()
+    var body: some View {
+            List(Restrunts, id: \.name) { rest in
+                HStack{
+                                        AsyncImage(url: rest.logo) { image in
+                                            image.resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(maxWidth: 80, maxHeight: 80)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                    Spacer()
+                    Text(rest.free).font(.title.bold())
+                }
+                Spacer()
+            }
+            .task {
+                await loadData()
+            }
+        
+    }
+    func loadData() async {
+        // func might want to go to sleep in order to complete work
+        guard let url = URL(string: "https://m.cpl.uh.edu/courses/ubicomp/fall2022/webservice/restaurant/restaurants.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonDecoder = JSONDecoder()
+                    Restrunts = try jsonDecoder.decode(Array<restrunts>.self, from: data)
+                    
+                } catch {
+                    print("Error trying to decode JSON object")
+                }
+                
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
     }
 }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
---
+++ Exercise5_waobikeze_Brian/RestaurantModel.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantModel.swift
+//  Exercise5_waobikeze_Brian
+//
+//  Created by brian waobikeze on 9/30/23.
+//
+
+import Foundation
+
+
+
+struct restrunts:Codable{
+    var name: String
+    var free: String
+    var phone: String
+    var logo: URL
+    var map: URL
+    var lots: String
+    var about: String
+}
